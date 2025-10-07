ff0c465a0f0ba74675649932605df58a2f861583
AjayKumar0507 <ajay200457@gmail.com>
2025-10-07 16:32:06 +0530

    Add project files.

---
+++ SchoolHRAdministration/Program.cs
@@ -0,0 +1,17 @@
+﻿using HRAdministrationAPI;
+using System;
+
+namespace HRAdministration {     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Teacher: EmployeeBase
+    {
+
+    }
+}
