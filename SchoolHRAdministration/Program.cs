b661ffbc2b9eb92022db21bf8154d3fb8a039858
AjayKumar0507 <ajay200457@gmail.com>
2025-10-07 22:56:31 +0530

    minor changes

--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -7,7 +7,54 @@ namespace HRAdministration {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Welcome to School HR Administration System");
+        }
+
+        public static void seedData(List<IEmployee> employees)
+        {
+            IEmployee teacher1 = new Teacher
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Marley",
+                Salary = 40000
+            };
+            employees.Add(teacher1);
+
+            IEmployee teacher2 = new Teacher
+            {
+                Id = 2,
+                FirstName = "Jenny",
+                LastName = "Thomas",
+                Salary = 40000
+            };
+            employees.Add(teacher2);
+
+            IEmployee headOfDepartment = new HeadOfDepartment
+            {
+                Id = 3,
+                FirstName = "Joesph",
+                LastName = "Stalin",
+                Salary = 100000
+            };
+            employees.Add(headOfDepartment);
+
+            IEmployee deputyHeadMaster = new DeputyHeadMaster
+            {
+                Id = 4,
+                FirstName = "Harey",
+                LastName = "Styles",
+                Salary = 50000
+            };
+            employees.Add(deputyHeadMaster);
+
+            IEmployee headMaster = new HeadMaster
+            {
+                Id = 5,
+                FirstName = "Taylor",
+                LastName = "Swift",
+                Salary = 200000
+            };
+            employees.Add(headMaster);
         }
     }
 
